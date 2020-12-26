22b9d4bb88409033b8f547529a1c93180d75262b
zwh <843532380@qq.com>
2020-12-26 19:18:59 +0800

    plugin中提供了更多接口，现在后端只需要处理图片的绝对路径即可

--- django/app/plugin.py
+++ django/app/plugin.py
@@ -1,14 +1,31 @@
 # wait to be fullfilled
 from .models import student
 from .form import StudentForm
+from django.core.files import File
+
+def transferFile2Django(filedir):
+    return File(open(filedir,"rb"))
+
+def createStudentFromFiledir(id, name, filedir) -> student:
+    return student(id, name, transferFile2Django(filedir))
+
+def getFiledirFromStudentForm(s_form:StudentForm) -> str:
+    photo = s_form.cleaned_data["photo"]
+    return photo.path
+
+def getFiledirFromStudent(s:student) -> str:
+    photo = s.photo
+    return photo.path
 
 def existStudentById(id:int) -> bool:
     return True
 
 def getStudentByName(Name:str) -> list:
+    # use createStudentFromFiledir(id, name, filedir) to create student
     return []
 
 def getStudentById(id:int) ->  student:
+    # use createStudentFromFiledir(id, name, filedir) to create student
     return student(id=1,name ="alice",photo ="NIL")
 
 def getStudentByIdAndName(id:int, name:str) ->  list:
@@ -17,19 +34,21 @@ def getStudentByIdAndName(id:int, name:str) ->  list:
     s = getStudentById(id)
     if s and s not in l:
         l.append(s)
+
+    # use createStudentFromFiledir(id, name, filedir) to create student
     return [student(id=1,name ="alice",photo ="photos/alice.png"),student(id=2,name ="tom",photo ="NIL")]
 
 def createStudent(s_form:StudentForm) -> bool:
     id = s_form.cleaned_data["id"]
     name = s_form.cleaned_data["name"]
-    photo = s_form.cleaned_data["photo"]
+    photodir = getFiledirFromStudentForm(s_form)
 
     return True
 
 def updateStudent(s_form:StudentForm) -> bool:
     id = s_form.cleaned_data["id"]
     name = s_form.cleaned_data["name"]
-    photo = s_form.cleaned_data["photo"]
+    photodir = getFiledirFromStudentForm(s_form)
     
     return True
     
